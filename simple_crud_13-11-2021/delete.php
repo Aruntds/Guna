7c4a34ca378fcd6b8be06a68d1eb936e457230c1
Aruntds <94176739+Aruntds@users.noreply.github.com>
2021-11-13 10:07:09 +0530

    Update delete.php

--- simple_crud_13-11-2021/delete.php
+++ simple_crud_13-11-2021/delete.php
@@ -1,4 +1,6 @@
 <?php 
+
+
 include 'connection.php';
 if(isset($_GET['delete_id'])){
  $id = $_GET['delete_id'];
@@ -6,7 +8,7 @@ if(isset($_GET['delete_id'])){
  if($query){
   header("location:index.php");
  }else{
-  echo "<script>alert('Sorry123')</script>";
+  echo "<script>alert('Sorry1232323')</script>";
  }
 }
 
